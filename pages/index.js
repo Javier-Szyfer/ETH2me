e59af18c990b5032eaee5a1d72e65b0189dde75f
Javier Szyfer <jszyfer@gmail.com>
2021-09-06 19:17:55 -0300

    v1

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,93 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useState } from "react";
+import { ethers } from "ethers";
+import TxList from "./../components/TxList";
+
+const createTransaction = async ({ setError, setTxs, ether, addr }) => {
+  console.log(ether, addr);
+
+  try {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+
+    await window.ethereum.sendAsync("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    ethers.utils.getAddress(addr);
+    const tx = await signer.sendTransaction({
+      to: addr,
+      value: ethers.utils.parseEther(ether),
+    });
+    console.log({ ether, addr });
+    console.log("tx", tx);
+    setTxs([tx]);
+  } catch (err) {
+    setError(err.message);
+  }
+};
 
 export default function Home() {
+  const [error, setError] = useState();
+  const [ether, setEther] = useState();
+  const [txs, setTxs] = useState([]);
+
+  const addr = "0xC0cc6807941Df9381Ac22F0272AAE10E01423d5e";
+
+  const handleSubmitPayment = async (e) => {
+    e.preventDefault();
+    setError();
+    await createTransaction({
+      setError,
+      setTxs,
+      ether,
+      addr,
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>send-eth</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <h1 className="text-6xl font-bold">ETH2me</h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <p className="mt-3 text-2xl bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+          Clone this repo
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form onSubmit={handleSubmitPayment} className="m-4">
+          <div className="flex flex-wrap  flex-col items-center justify-around max-w-4xl mt-6 sm:w-full ">
+            <input
+              type="text"
+              className="border-solid border-4 border-light-blue-500 max-w-md my-3"
+              placeholder="Amount in ETH"
+              onChange={(e) => setEther(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="py-2 px-8 bg-red-300 rounded-full text-xl font-semibold"
+            >
+              Pay
+            </button>
+          </div>
+        </form>
       </main>
+      <TxList txs={txs} />
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      {/* <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
-      </footer>
+      </footer> */}
     </div>
-  )
+  );
 }
